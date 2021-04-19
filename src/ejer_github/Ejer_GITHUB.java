bb0d510f8d71fb72c9cb51174e41a2762dc604d5
linom <linom@DESKTOP-K52B94G>
2021-04-19 12:43:42 -0600

    Programa despliega saludo

---
+++ src/ejer_github/Ejer_GITHUB.java
@@ -0,0 +1,15 @@
+
+package ejer_github;
+
+
+public class Ejer_GITHUB {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here       
+        System.out.println("Hola grupo de POO");
+        
+        
+    }
+    
+}
